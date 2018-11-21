03f80355a6a15865a77ea70bd732a4c437ad203a
shameekASC5 <shameekhargrave@gmail.com>
2018-11-21 18:20:04 -0500

    Portfolio Update

---
+++ scriptProject.js
@@ -0,0 +1,29 @@
+function paintTool()
+{
+    window.location = "https://shameekhargrave.com/paintTool/";
+}
+
+function virtualCookie()
+{
+    window.location = "https://shameekhargrave.com/Fortune/";
+}
+
+function calculator()
+{
+    window.location = "https://shameekhargrave.com/Calculator/";
+}
+
+function hackathon()
+{
+    window.location = "https://shameekhargrave.com/Hackathon8_9/";
+}
+
+function meekPong()
+{
+    window.location = "https://shameekhargrave.com/Meek-Pong/";
+}
+
+function fortune()
+{
+    window.location = "https://shameekhargrave.com/Fortune/";
+}
